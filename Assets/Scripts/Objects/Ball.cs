de19dcb4a05bbd4bcf19193671665ea1770ebdcc
NanamiDiana <didiana.g.a@gmail.com>
2025-04-11 20:18:51 +0100

    Points Manager + inicio ball

--- Assets/Scripts/Objects/Ball.cs
+++ Assets/Scripts/Objects/Ball.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class Ball
 {
-    
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    /// <summary>       VER AQUI
+    /// /public int Player { set; get actualPlayer }; //isso nao deve tar bem  escrito xd
+    /// </summary>
+
+    private int actualPlayer = 0;
+
+    //quando o player lança a bola envia aqui o seu numero
+
+    // public lançar bola (int playernumber)
+    // { actual player = player numebr }
+    // 
 }
--- Assets/Scripts/Objects/Points_Manager.cs
+++ Assets/Scripts/Objects/Points_Manager.cs
@@ -1,6 +1,47 @@
 using UnityEngine;
 
-public class PointsManager
+public class GamePlayManager
 {
-    
+    private int scorePoints = 0;
+    private int ActualPoints = 0;
+
+    private bool isRunning = false;
+
+    [SerializeField] private Collider floorCollider;
+    [SerializeField] private Collider bootomCollider;
+    [SerializeField] private Collider frontCollider;
+    [SerializeField] private Collider leftCollider;
+    [SerializeField] private Collider rightCollider;
+
+    private int[] players = new int[2] { 1, 2 };
+
+    public void TouchingGround()
+    {
+        if (isRunning)
+        {
+            isRunning = false;
+        }
+        else
+        {
+            isRunning = true;
+        }
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+
 }

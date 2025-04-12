3b89922fe006eb6e4e4ab1a533f4318f6144344f
NanamiDiana <didiana.g.a@gmail.com>
2025-04-12 12:56:06 +0100

    v12:56

---
+++ Assets/Scripts/Effects/GroundOut.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GroundOut : MonoBehaviour
+{
+    //tremer e dps cair - aniamtor
+
+    //guaradr posicao inciatl
+    //NamespaceOption é afetado pela grvaidade
+
+    [SerializeField] private GameObject[] leftGround;
+    [SerializeField] private GameObject[] rightGround;
+
+    
+    private 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
